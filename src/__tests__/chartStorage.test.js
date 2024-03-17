0f960cce2f5187f6fd1bea6230c3692d7c0f60d7
victorgp <victorgpoetra@gmail.com>
2024-03-17 14:13:46 -0700

    Updated chartStorage

--- src/__tests__/chartStorage.test.js
+++ src/__tests__/chartStorage.test.js
@@ -36,32 +36,46 @@ beforeEach(() => {
     window.localStorage.clear();
 });
 
-test('saveChart should save a new chart and loadAllSavedCharts should load it', () => {
-    const chart = { id: '1', data: [10, 20, 30] };
-    saveChart(chart);
-    const charts = loadAllSavedCharts();
-    expect(charts).toEqual([chart]);
+test('saveChart should save a new chart at the end', () => {
+  const chart1 = { id: '1', data: [20, 30] };
+  const chart2 = { id: '2', data: [50, 60] };
+  saveChart(chart1);
+  saveChart(chart2);
+  const chart = loadSavedChart(1);
+  expect(chart).toEqual(chart2);
 });
 
 test('saveChart should overwrite an existing chart at specified index', () => {
-    const chart1 = { id: '1', data: [10, 20, 30] };
-    const chart2 = { id: '2', data: [40, 50, 60] };
-    saveChart(chart1);
-    saveChart(chart2, 0); // Overwrite chart at index 0
-    const charts = loadAllSavedCharts();
-    expect(charts).toEqual([chart2]);
+  const chart1 = { id: '1', data: [20, 30] };
+  const chart2 = { id: '2', data: [50, 60] };
+  saveChart(chart1);
+  saveChart(chart2, 0);
+  const charts = loadAllSavedCharts();
+  expect(charts).toEqual([chart2]);
+});
+
+test('loadAllSavedChart should return all saved charts', () => {
+  const charts = [{ id: '1', data: [20, 30] }, { id: '2', data: [50, 60] }];
+  const chart1 = { id: '1', data: [20, 30] };
+  const chart2 = { id: '2', data: [50, 60] };
+  saveChart(chart1);
+  saveChart(chart2);
+  const allCharts = loadAllSavedCharts();
+  expect(allCharts).toEqual(charts);
 });
 
 test('loadSavedChart should load a specific chart by index', () => {
-    const chart = { id: '1', data: [10, 20, 30] };
-    saveChart(chart);
-    const loadedChart = loadSavedChart(0);
-    expect(loadedChart).toEqual(chart);
+  const chart1 = { id: '1', data: [20, 30] };
+  const chart2 = { id: '2', data: [50, 60] };
+  saveChart(chart1);
+  saveChart(chart2);
+  const loadedChart = loadSavedChart(0);
+  expect(loadedChart).toEqual(chart1);
 });
 
 test('updateCurrentChartData should update and loadCurrentChartData should load the current chart data', () => {
-    const currentChartData = { type: 'bar', data: [1, 2, 3] };
-    updateCurrentChartData(currentChartData);
-    const loadedData = loadCurrentChartData();
-    expect(loadedData).toEqual(currentChartData);
+  const currentChartData = { type: 'bar', data: [2, 3] };
+  updateCurrentChartData(currentChartData);
+  const loadedData = loadCurrentChartData();
+  expect(loadedData).toEqual(currentChartData);
 });
